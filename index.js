5b1241b7f501003e4547dd9217ff7bf2c734fbb9
RaidEyes <phamquanghung199713@gmail.com>
2024-03-11 21:37:41 +0700

    initialize routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
+import postalCodes from "./src/routes/postalCodes.js";
 
 // express app
 const app = express();
@@ -10,6 +10,9 @@ dotenv.config();
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// routes
+app.use("/", postalCodes);
 
 // Listen port
 app.listen(process.env.PORT, () => {
